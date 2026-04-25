343517c59fd9ab625aae5d46e3704392abaaad02
priyanshu-573 <priyanshu.bohra573@gmail.com>
2026-04-26 02:52:14 +0530

    Add geolocation & forecast

--- js/script.js
+++ js/script.js
@@ -6,38 +6,113 @@ const cityInput = document.getElementById("cityInput");
 const weatherIcon = document.querySelector(".weather-icon");
 const loader = document.querySelector(".loader");
 const weather = document.querySelector(".weather");
+const locationBtn = document.getElementById('location')
 
+// cityInput.addEventListener("keypress",(e)=>{
+//     if(e.key==="Enter"){
+//         searchWeather();
+//     }
+// });
+const form = document.getElementById("searchForm");
 
-async function fetchWeather(city) {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchWeather();
+});
 
-    
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    const forecastResponse = await fetch(forecastUrl + city + `&appid=${apiKey}`);
+locationBtn.addEventListener('click', (e) => {
+    navigator.geolocation.getCurrentPosition(
+        showPosition,
+        (error) => {
+            if(error.code === 1){
+                alert('Permission denied');
+            }else{
+                alert('Unable to get Location');
+            }
+        }
+    );
+});
 
-    if (response.status === 404) {
-        alert("City not found");
-        return;
-    }
+async function showPosition(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
 
-    const data = await response.json();
-    const forecastData = await forecastResponse.json();
+    loader.style.display = "block";
+    weather.style.display = "none";
 
-    return { data, forecastData };
-}
+    const data = await fetchWeatherByCoord(lat, lon);
+    const forecastData = await fetchForecastByCoord(lat, lon);
 
-    async function checkWeather(city) {
-
-        loader.style.display = "block";
-        weather.style.display = "none";
-
-
-    const result = await fetchWeather(city);
-
-    if (!result) {
+    if (!data || !forecastData) {
     loader.style.display = "none";
     return;
 }
 
+     updateUI(data);
+     displayHourlyForecast(forecastData);
+
+    loader.style.display = "none";
+    weather.style.display = "block";
+}
+
+async function fetchWeatherByCoord(lat, lon) {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+    );
+
+    if (!response.ok) {
+        alert("Error fetching weather");
+        return;
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+async function fetchForecastByCoord(lat, lon) {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+    );
+
+    if (!response.ok) {
+        alert("Error fetching forecast");
+        return;
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+async function fetchWeather(city) {
+    
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const forecastResponse = await fetch(forecastUrl + city + `&appid=${apiKey}`);
+
+    if (!response.ok) {
+        alert("City not found");
+        return;
+    }
+
+    if (!forecastResponse.ok) {
+    alert("Forecast error");
+    return;
+}
+
+    const data = await response.json();
+    const forecastData = await forecastResponse.json();
+    return {data, forecastData};
+}
+
+async function checkWeather(city) {
+    loader.style.display = "block";
+    weather.style.display = "none";
+
+    const result = await fetchWeather(city);
+
+    if (!result || !result.data || !result.forecastData) {
+    loader.style.display = "none";
+    return;
+}
     const { data, forecastData } = result;
 
     updateUI(data);
@@ -45,9 +120,10 @@ async function fetchWeather(city) {
 
     loader.style.display = "none";
     weather.style.display = "block";
+
 }
     
-
+    
         function updateUI(data) {
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -72,6 +148,7 @@ async function fetchWeather(city) {
     } else {
         weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     }
+    cityInput.value=""
         }
 
         
@@ -97,7 +174,13 @@ function displayHourlyForecast(data) {
 
 
 function searchWeather() {
-    checkWeather(cityInput.value);
+    const city = cityInput.value.trim();
+
+    if(!city){
+        alert("Enter a city name");
+        return;
+    }
+    checkWeather(city);
 }
 
 // default city ------
